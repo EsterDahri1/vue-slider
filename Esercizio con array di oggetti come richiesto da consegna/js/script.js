b7805c7530dfcab56c09a82f07a3ae6dfed32685
EsterDahri <esterdahri@gmail.com>
2023-09-12 19:27:07 +0200

    add prev and next function + fix typo activeImg

--- Esercizio con array di oggetti come richiesto da consegna/js/script.js
+++ Esercizio con array di oggetti come richiesto da consegna/js/script.js
@@ -17,23 +17,23 @@ const {createApp} = Vue
 
                 slides: [
                     {
-                        image: 'img/01.webp',
+                        image: './img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                     }, {
-                        image: 'img/02.webp',
+                        image: './img/02.webp',
                         title: 'Ratchet & Clank: Rift Apart',
                         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                     }, {
-                        image: 'img/03.webp',
+                        image: './img/03.webp',
                         title: 'Fortnite',
                         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                     }, {
-                        image: 'img/04.webp',
+                        image: './img/04.webp',
                         title: 'Stray',
                         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                     }, {
-                        image: 'img/05.webp',
+                        image: './img/05.webp',
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                     }
@@ -42,7 +42,29 @@ const {createApp} = Vue
             }
         },
 
-       
+        //funzioni
+        methods: {
+
+            //pulsante per passare alla prossima slide
+            next() {
+                this.activeImg++;
+                if (this.activeImg > this.slides.length - 1) {
+                    this.activeImg = 0;
+                }
+            },
+
+            //pulsante per passare alla precedente slide
+            prev() {
+                this.activeImg--;
+                if (this.activeImg < 0) {
+                    this.activeImg = this.slides.length - 1;
+                }
+            },
+
+            activeClass(index){
+                return `thumb ${this.activeImg === index - 1 ? 'active' : ''}`
+            }
+        },
 
     }).mount('#app')
 
